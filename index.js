f8f6454456741695787dccd2db4fdfbb7db71720
Cropgen56 <cropgenapp@gmail.com>
2024-12-12 20:45:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const startServer = async () => {
 
     // Start the server
     const PORT = process.env.PORT || 5000;
-    app.listen(PORT, () => {
+    app.listen(PORT,"0.0.0.0",() => {
       console.log(`Server running on http://localhost:${PORT}`);
     });
   } catch (error) {
