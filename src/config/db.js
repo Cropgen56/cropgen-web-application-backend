d8b3978c3a6247ae9554a7e759d1ae1c048a3796
vishalpatilmaske <vishpatilmaske@gmail.com>
2024-12-12 18:09:39 +0530

    make the changes in the db file

--- src/config/db.js
+++ src/config/db.js
@@ -19,7 +19,6 @@ const sequelize = new Sequelize(
 const testConnection = async () => {
   try {
     await sequelize.authenticate();
-    console.log("Database connected successfully.");
   } catch (error) {
     console.error("Unable to connect to the database:", error);
   }
