9327958d66632e837e737ddca08d2af165be8262
vishalpatilmaske <vishalpatilmaske@gmail.com>
2025-11-16 10:39:55 +0530

    remove the authentication form the operation

--- src/routes/operationRoutes.js
+++ src/routes/operationRoutes.js
@@ -12,10 +12,10 @@ import { isAuthenticated } from "../middleware/authMiddleware.js";
 const router = express.Router();
 
 // Define the routes
-router.post("/:farmFieldId/create", isAuthenticated, addOperation);
-router.get("/:farmFieldId/get", isAuthenticated, getOperationsByFarmField);
-router.get("/:operationId", isAuthenticated, getOperationById);
-router.put("/:operationId", isAuthenticated, updateOperation);
-router.delete("/:operationId", isAuthenticated, deleteOperation);
+router.post("/:farmFieldId/create", addOperation);
+router.get("/:farmFieldId/get", getOperationsByFarmField);
+router.get("/:operationId", getOperationById);
+router.put("/:operationId", updateOperation);
+router.delete("/:operationId", deleteOperation);
 
 export default router;
